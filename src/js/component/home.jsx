fa9453455a489d398df6e5783f6bcdff123bc571
Matías Quezada <matias.traducciones@gmail.com>
2021-10-08 12:48:56 -0300

    jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
 	);
 };
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">A Warm Welcome!</h1>
+		<p class="lead">
+		This is a modified jumbotron that
+		occupies the entire horizontal
+		space of its parent.</div>
+
+</div>
 
 export default Home;
